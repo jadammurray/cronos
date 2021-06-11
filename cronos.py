3e8b3eeb56d3cf6a144c263d58588e066a1d9734
jadammurray <jadam@gardenway.org>
2021-06-11 06:48:58 -0700

    Add logging

--- cronos.py
+++ cronos.py
@@ -1,9 +1,36 @@
 
 import os
+from logging import debug
+from logging.config import dictConfig
 import ezgmail
 
 vasometrics_path = "v:\\Vasometrics"
 clinic_list = os.listdir(vasometrics_path)
+
+dictConfig({
+    'version': 1,
+    'formatters': {
+        'default': {
+            'format': '[%(asctime)s] [%(levelname)s] [%(filename)s(%(lineno)d)] [PID:%(process)d]: %(message)s',
+        }
+    },
+    'handlers': {
+        'file': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'default',
+            'maxBytes': 100000000,
+            'backupCount': 5,
+            'filename': '0.cronos.log',
+            'mode': 'w',
+        },
+    },
+    'root': {
+        'level': 'DEBUG',
+        'handlers': ['file']
+    }
+})
+
+debug('first debug: {}'.format(5))
 
 for clinic in clinic_list:
     #### Skip these files/directories
